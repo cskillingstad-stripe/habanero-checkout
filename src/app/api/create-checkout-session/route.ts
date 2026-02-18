349988fdcc42dbf8aab1c940f7872e1b538bda69
CJ Skillingstad <cskillingstad@stripe.com>
2026-02-18 09:32:37 -0700

    clover backend

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,9 +1,9 @@
-import {NextResponse} from 'next/server';
+import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
-import {ITEMS, SHIPPING_OPTIONS} from '@/constants';
+import { ITEMS, SHIPPING_OPTIONS } from '@/constants';
 
 const stripe = new Stripe(process.env.STRIPE_TEST_SK!, {
-  apiVersion: '2025-08-27.basil;custom_checkout_payment_form_preview=v1',
+  apiVersion: '2025-09-30.clover;custom_checkout_payment_form_preview=v1',
 });
 
 // const calculateOrderAmount = (_items: any) => {
@@ -166,12 +166,12 @@ export async function POST(request: Request) {
       return_url: `${request.headers.get('origin')}/complete?session_id={CHECKOUT_SESSION_ID}`,
     });
 
-    return NextResponse.json({clientSecret: session.client_secret});
+    return NextResponse.json({ clientSecret: session.client_secret });
   } catch (error) {
     console.error('Error creating checkout session:', error);
     return NextResponse.json(
-      {error: 'Error creating checkout session'},
-      {status: 500}
+      { error: 'Error creating checkout session' },
+      { status: 500 }
     );
   }
 }
